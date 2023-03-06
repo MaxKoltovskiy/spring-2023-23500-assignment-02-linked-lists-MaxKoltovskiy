46e25674dafc47d9a87747336eff29889c6a5229
Mac Koltovskiy <mackolt@DESKTOP-P2PHME8.localdomain>
2023-03-06 08:35:36 -0500

    OList

---
+++ OList.cpp
@@ -0,0 +1,102 @@
+#include "OList.h"
+
+OList::OList() {
+    head = nullptr;
+}
+
+OList::~OList() {
+    Node *walker = head;
+    Node *trailer = nullptr;
+
+    while(walker != nullptr) {
+        trailer = walker;
+        walker = walker -> getNext();
+        delete trailer;
+    }
+}
+
+void OList::insert(std::string value) {
+    Node *walker = head;
+    Node *trailer = nullptr;
+    while(walker != nullptr && (walker -> getData()) < value) {
+        trailer = walker;
+        walker = walker -> getNext();
+
+    }
+
+    Node *newNode = new Node(value);
+    if(trailer == nullptr) {
+        newNode -> setNext(head);
+        head = newNode;
+    } else if(walker == nullptr) {
+        trailer -> setNext(newNode);
+    } else {
+        trailer -> setNext(newNode);
+        newNode -> setNext(walker);
+    }
+}
+
+std::string OList::toString() {
+    Node *walker = head;
+    std::string result;
+    
+    while(walker != nullptr) {
+        result += walker -> getData() + " --> ";
+        walker = walker -> getNext();
+    }
+
+    result += "nullptr\n";
+    return result;
+}
+
+bool OList::contains(std::string value) {
+    Node *walker = head;
+    while(walker != nullptr) {
+        if(value == walker -> getData()) {
+            return true;
+        }
+
+        walker = walker -> getNext();
+    }
+
+    return false;
+}
+
+std::string OList::get(int loc) {
+    Node *walker = head;
+    for(int i = loc; i> 0; i--) {
+        if(walker != nullptr) {
+            walker = walker -> getNext();
+        }
+    }
+
+    if(walker == nullptr) {
+        return "Past Range";
+    }
+
+    return walker -> getData();
+}
+
+void OList::remove(int loc) {
+    Node *walker = head;
+    Node *trailer = nullptr;
+    for(int i = loc; i> 0; i--) {
+        if(walker != nullptr) {
+            trailer = walker;
+            walker = walker -> getNext();
+        }
+    }
+
+    if(walker == nullptr) {
+        return;
+    }
+
+    if(trailer == nullptr) {
+        head = walker -> getNext();
+        delete walker;
+    } else {
+        trailer -> setNext(walker -> getNext());
+        delete walker;
+    }
+    
+}
---
+++ OList.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Node.h"
+
+class OList {
+    private:
+        Node *head;
+    public:
+        OList();
+        ~OList();
+
+        void insert(std::string value);
+        std::string toString();
+        bool contains(std::string value);
+
+        std::string get(int loc);
+        void remove(int loc);
+
+        void reverse();
+};
