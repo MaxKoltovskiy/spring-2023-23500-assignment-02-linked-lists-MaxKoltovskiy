fbd8479641325f696114e228839d844b39b0da9f
Mac Koltovskiy <mackolt@DESKTOP-P2PHME8.localdomain>
2023-03-06 08:35:05 -0500

    OList

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJECTS = List.o Node.o main.o 
+OBJECTS = List.o Node.o main.o OList.o 
 
 main: $(OBJECTS)
 	g++ -o main $(OBJECTS)
@@ -6,6 +6,7 @@ main: $(OBJECTS)
 List.o: List.cpp List.h Node.h
 Node.o: Node.cpp Node.h
 main.o: main.cpp List.h Node.h
+OList.o: OList.cpp OList.h Node.h
 
 clean:
 	rm -f $(OBJECTS)
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "List.h"
+#include "OList.h"
 
 int main() 
 {
@@ -16,8 +17,37 @@ int main()
     std::cout<< l -> toString() << "\n\n";
 
     delete l;
-    std::cout<< "toString after deletion: "<< "\n";
-    std::cout<<  l -> toString() << "\n";
+
+    std::cout<< "------------------------------\n\n";
+
+    OList *o = new OList();
+    o -> insert("b");
+    std::cout<< o -> toString();
+    o -> insert("a");
+    std::cout<< o -> toString();
+    o -> insert("d");
+    std::cout<< o -> toString();
+    o -> insert("c");
+    std::cout<< o -> toString();
+    
+    std::cout<< "contains(a): " << o -> contains("a")<< "\n";
+    std::cout<< "contains(c): " << o -> contains("c")<< "\n";
+    std::cout<< "contains(e): " << o -> contains("e")<< "\n";
+    
+    std::cout<< "get(0): " << o -> get(0)<< "\n";
+    std::cout<< "get(2): " << o -> get(2)<< "\n";
+    std::cout<< "get(4): " << o -> get(4)<< "\n";
+
+    o -> remove(0);
+    std::cout<< o -> toString();
+    o -> remove(3);
+    std::cout<< o -> toString();
+    o -> remove(2);
+    std::cout<< o -> toString();
+
+
+
+    
 
 
     return 0;
